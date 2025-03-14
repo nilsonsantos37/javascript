e5f95793e8fca48ae5c363bfcfda7a377dd86f6f
Nilson dos Santos <83931804+nilsonsantos37@users.noreply.github.com>
2025-03-14 18:07:21 -0300

    criei a aula 13 e 14

---
+++ aula13/ambiente.js
@@ -0,0 +1,22 @@
+var c = 1;
+do {
+    console.log(`Passo ${c}`);
+    c++
+} while (c <= 20)
+
+/*
+var c = 1;
+while (c <= 300) {
+    console.log(`Passo ${c}`);
+    c++
+}
+*/ 
+
+/*
+console.log('Tudo bem?');
+console.log('Tudo bem?');
+console.log('Tudo bem?');
+console.log('Tudo bem?');
+console.log('Tudo bem?');
+console.log('Tudo bem?');
+*/
